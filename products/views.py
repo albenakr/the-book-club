816b89e2ea5ddc342ec526ccd4515e3f6d746cbd
Albena Kr <albena.krasteva@gmail.com>
2020-08-23 14:01:12 +0000

    Added products views,templates and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def all_books(request):
+    """ A view for all books, searching and filtering"""
+
+    books = Book.objects.all()
+
+    context = {
+        'books': books,
+    }
+
+    return render(request, 'products/books.html', context)
