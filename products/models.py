d1a782b6d9ea03f1d996c5fbd7be275735319897
Albena Kr <albena.krasteva@gmail.com>
2020-08-23 13:34:33 +0000

    Added product fixtures for genre, languages and books

--- products/models.py
+++ products/models.py
@@ -23,9 +23,9 @@ class Book(models.Model):
     genre = models.ForeignKey('Genre', null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=454)
     author = models.CharField(max_length=454)
-    publ_year = models.IntegerField(validators=[MinValueValidator(1000), MaxValueValidator(2020)], null=True, blank=True)
+    publication_year = models.IntegerField(validators=[MinValueValidator(1000), MaxValueValidator(2020)], null=True, blank=True)
     book_description = models.TextField()
-    price = price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
