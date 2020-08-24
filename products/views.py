2f8c44e59c72779581dcfd1687b8bde85755b776
Albena Kr <albena.krasteva@gmail.com>
2020-08-24 17:04:42 +0000

    Added search functionality

--- products/views.py
+++ products/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Book
 
-# Create your views here.
 
 def all_books(request):
     """ A view for all books, searching and filtering"""
 
     books = Book.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search keywords")
+                return redirect(reverse('books'))
+
+            queries = Q(title__icontains=query) | Q(book_description__icontains=query) | Q(author__icontains=query)
+            books = books.filter(queries)
 
     context = {
         'books': books,
+        'search_term': query,
     }
 
     return render(request, 'products/books.html', context)
