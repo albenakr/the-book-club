1430da03cb9a8c03bdb94655b5fa6d816ca00281
Albena Kr <albena.krasteva@gmail.com>
2020-09-08 20:28:00 +0000

    updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,26 @@
-def bag_contents(request):
+from django.shortcuts import get_object_or_404
+from products.models import Book
 
+
+def bag_contents(request):
     bag_items = []
-    total = 0
+    price_total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        book = get_object_or_404(Book, pk=item_id)
+        price_total += quantity * book.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'book': book,
+        })
 
     context = {
         'bag_items': bag_items,
-        'total': total,
+        'price_total': price_total,
         'product_count': product_count,
     }
 
