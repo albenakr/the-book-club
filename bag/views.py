a7cd8cc4a68873db0c66f35d46b378641c6bee9e
Albena Kr <albena.krasteva@gmail.com>
2020-09-20 09:24:49 +0000

    Added Toasts for displaying messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Book
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of a specified product to the shopping bag"""
 
+    book = Book.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
@@ -20,6 +23,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'{book.title} was added to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
