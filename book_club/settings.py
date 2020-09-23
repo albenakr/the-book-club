79ceba79101a277ebcea5b07f1365f97bd93800c
Albena Kr <albena.krasteva@gmail.com>
2020-09-23 17:51:09 +0000

    Added plans to the checkout template, fixed checkout error, added image for all plans

--- book_club/settings.py
+++ book_club/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'plans',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'book_club.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 TEMPLATES = [
     {
@@ -76,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
