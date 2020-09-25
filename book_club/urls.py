01b9f8863c80a0a6874c36e0d903ad75db11b31e
Albena Kr <albena.krasteva@gmail.com>
2020-09-25 15:26:32 +0000

    Issue with profile functionality, bug to be fixed

--- book_club/urls.py
+++ book_club/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('plans/', include('plans.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
