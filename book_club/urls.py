9c5e4dc8fb9bb3d56dd4e2e15de9a94ccc4bab0f
Albena Kr <albena.krasteva@gmail.com>
2020-09-26 12:34:18 +0000

    Added url for when a customer lands on a 404 page, they get redirected to the home page

--- book_club/urls.py
+++ book_club/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
 
 urlpatterns = [
@@ -29,4 +30,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
+    url(r'^.*/$', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
