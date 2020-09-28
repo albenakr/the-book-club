498d8841f079dd62af49bf425f240976da13dda9
Albena Kr <albena.krasteva@gmail.com>
2020-09-28 18:24:03 +0000

    Adding 'back to top version to project level static and base.html- this version is not working

---
+++ static/js/back_to_top_btn.js
@@ -0,0 +1,20 @@
+/* Back to top button from W3 School - details in README*/
+//Get the button
+var backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
