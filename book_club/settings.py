45509b4241cfb78afd7dc18f7d61816f3c6cecea
Albena Kr <albena.krasteva@gmail.com>
2020-10-02 16:54:10 +0000

    Removed secret key and set debug to true in development

--- book_club/settings.py
+++ book_club/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd6uz&1u3qk!0!e@#955tsx5oitj!z4!45^xa7&lu(7p6!8o)c#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['book-club-ecommerce.herokuapp.com']
+ALLOWED_HOSTS = ['book-club-ecommerce.herokuapp.com', 'localhost']
 
 
 # Application definition
