08563f3021c2d8cca08e75d4411af85b9fef20dc
Albena Kr <albena.krasteva@gmail.com>
2020-10-03 09:49:38 +0000

    Tidied up code in the views, added comments, deleted print statements

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404)
 from django.db.models import Avg
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -15,35 +16,28 @@ def write_review(request, book_id):
     """A view to allow a user to create a review"""
 
     book = get_object_or_404(Book, pk=book_id)
-    print(book)
     user = get_object_or_404(UserProfile, user=request.user)
-    print(user)
 
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = ReviewForm(request.POST)
 
-        # check whether it's valid:
         if form.is_valid():
             rating = form.cleaned_data['rating']
-            print(rating)
             title = form.cleaned_data['title']
-            print(title)
             review_text = form.cleaned_data['review_text']
-            print(review_text)
 
-            print(user)
-
-            review = Review(book=book, user_profile=user, rating=rating, title=title,
-                            review_text=review_text)
+            review = Review(
+                book=book, user_profile=user,
+                rating=rating, title=title, review_text=review_text)
             review.save()
             messages.success(
                 request, 'Your review was successfully posted.')
 
             return redirect('book_detail', book_id=book.id)
+
         else:
             messages.error(request, 'There was an error while submitting your review. \
-                Your title or review might be too long.')
+                Your title or review text might be too long.')
 
             context = {
                 'form': form,
@@ -55,7 +49,6 @@ def write_review(request, book_id):
             template = 'reviews/write_review.html'
 
             return render(request, template, context)
-            # return HttpResponse(status=500)
 
     else:
         form = ReviewForm()
@@ -73,6 +66,7 @@ def write_review(request, book_id):
 
 
 def calculate_average_rating(book_id):
+    """ Calculate average review rating per book """
     book = get_object_or_404(Book, pk=book_id)
     all_reviews = Review.objects.all()
     reviews = all_reviews.filter(book=book)
@@ -81,10 +75,9 @@ def calculate_average_rating(book_id):
 
 
 def delete_review(request, review_id):
-    """ Delete a review, only available on profile page for the user who created the reviw """
+    """ Delete a review, currently
+    only available on profile page for the user, who created the review """
     review = get_object_or_404(Review, pk=review_id)
     review.delete()
     messages.success(request, 'Your review was deleted!')
     return redirect(reverse("profile"))
-
-
