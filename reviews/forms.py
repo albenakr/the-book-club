62e314988a4d0e18bb4a6a2bb04417627175b509
Albena Kr <albena.krasteva@gmail.com>
2020-10-04 20:20:32 +0000

    Updated labels and styling on the  reviews form

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,3 +6,8 @@ class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields = ['rating', 'title', 'review_text']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['review_text'].label = "Your Review"
+
