1364659364356a1cc400ed50b75cd644a117b5b7
Albena Kr <albena.krasteva@gmail.com>
2020-10-04 22:35:11 +0000

    Updated styling of plan form

--- plans/forms.py
+++ plans/forms.py
@@ -1,17 +1,17 @@
 from django import forms
 from products.models import Genre, Language
 
+
 class CustomPlanForm(forms.Form):
     CHOICES = (
         (3, '3 months'),
         (6, '6 months'),
         (12, '12 months'),
     )
-    name = forms.CharField(label='Name your plan', max_length=454)
+    name = forms.CharField(label='Name Your Plan', max_length=454)
     genres = forms.ModelMultipleChoiceField(
         widget=forms.CheckboxSelectMultiple, queryset=Genre.objects.all())
     languages = forms.ModelMultipleChoiceField(
         widget=forms.CheckboxSelectMultiple, queryset=Language.objects.all())
     plan_duration = forms.ChoiceField(choices=CHOICES)
-
 
